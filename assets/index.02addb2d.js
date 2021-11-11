40db5bab379ccebe76c70d1fde37df87756f6375
chimu23 <39324019+chimu23@users.noreply.github.com>
2021-11-11 15:50:51 +0000

    Deploying to gh-pages from @ chimu23/all@d892ba88e345f24f3e6c409244d6afee68964f43 🚀

---
+++ assets/index.02addb2d.js
@@ -0,0 +1,1 @@
+import{o as e,d as r}from"./vendor.dc60cad0.js";const n={setup(c){return(o,t)=>(e(),r("div",null,"fire123"))}};export{n as default};
--- assets/index.0b9442da.js
+++
@@ -1,1 +0,0 @@
-import{o as e,d as r}from"./vendor.dc60cad0.js";const n={setup(c){return(o,t)=>(e(),r("div",null,"fire"))}};export{n as default};
